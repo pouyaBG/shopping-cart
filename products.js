bb7d126d45bd61c2ec79011f51eddd11fb5ee8fc
pouya <b_pooya@yahoo.com>
2022-02-18 15:01:17 +0330

    product export

---
+++ products.js
@@ -0,0 +1,50 @@
+export const productsData = [
+  {
+    id: 1,
+    title: "Royal bed",
+    price: 154.14,
+    imageUrl: "./images/product-1.jpeg",
+  },
+  {
+    id: 2,
+    title: "Medical bed",
+    price: 198.11,
+    imageUrl: "./images/product-2.jpeg",
+  },
+  {
+    id: 3,
+    title: "Comfortable bed",
+    price: 98.99,
+    imageUrl: "./images/product-3.jpeg",
+  },
+  {
+    id: 4,
+    title: "Twin easy bed",
+    price: 45.55,
+    imageUrl: "./images/product-4.jpeg",
+  },
+  {
+    id: 5,
+    title: "fridge",
+    price: 20.12,
+    imageUrl: "./images/product-5.jpeg",
+  },
+  {
+    id: 6,
+    title: "fridge",
+    price: 23.99,
+    imageUrl: "./images/product-6.jpeg",
+  },
+  {
+    id: 7,
+    title: "dresser",
+    price: 42.99,
+    imageUrl: "./images/product-7.jpeg",
+  },
+  {
+    id: 8,
+    title: "couch",
+    price: 24.99,
+    imageUrl: "./images/product-8.jpeg",
+  },
+];
